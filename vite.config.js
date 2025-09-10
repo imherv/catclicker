301f124c7a51dd865c39b2b5efce20fa38a2a8d8
Herv <45630137+imherv@users.noreply.github.com>
2025-09-10 20:10:30 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // muito importante! garante paths relativos
+  base: './src', // muito importante! garante paths relativos
   plugins: [react()],
 });
